3b214ec362b5866f8c35b01c2205d8fd42f26de5
Lucian Ursu <lucian.ursu@gmail.com>
2024-12-17 21:31:12 +0200

    Refactor and use new error functions

--- src/jor.py
+++ src/jor.py
@@ -1,21 +1,26 @@
 import numpy as np
 
+from .error import step, residual
+
 
 def jor(A, b, x0, omega=1.0, tol=1e-6, max_iter=100, opt=0):
     """
     Jacobi Overrelaxation (JOR) with selectable error metric (OPT)
     """
     x = x0.copy()
-    D = np.diag(A)
-    R = A - np.diag(D)
+    #D = np.diag(A)
+    D_inv = np.diag(1 / np.diag(A))
+    #R = A - np.diag(D)
 
     for k in range(max_iter):
-        x_new = (1 - omega) * x + omega * (b - R @ x) / D
 
-        if opt == 0:  # Increment-based error
-            error = np.linalg.norm(x_new - x, ord=np.inf)
-        elif opt == 1:  # Residual-based error
-            error = np.linalg.norm(b - A @ x_new, ord=np.inf)
+        x_new = x + omega * (D_inv @ (b - A @ x) - x)
+        #x_new = (1 - omega) * x + omega * (b - R @ x) / D
+
+        if opt == 0:
+            error = step(x_new, x)
+        elif opt == 1:
+            error = residual(A, x_new, b)
         else:
             raise ValueError(
                 "Invalid OPT value. Use 0 (increment) or 1 (residual).")
