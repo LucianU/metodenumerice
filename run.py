ccd71467ffa78dad22f3dc701958e53345d0c204
Lucian Ursu <lucian.ursu@gmail.com>
2025-01-29 11:12:20 +0200

    Run more methods on more inputs

--- run.py
+++ run.py
@@ -5,23 +5,43 @@ from metodenumerice.jacobi import jacobi
 from metodenumerice.jor import jor
 from metodenumerice.gs import gs
 from metodenumerice.gsb import gsb
+from metodenumerice.sor import sor
 
 
 if __name__ == '__main__':
     x0 = np.zeros_like(data.b1)
 
-    x_jacobi = jacobi(data.A3, data.b3, x0)
-    print("Jacobi solution: ", x_jacobi)
+    print("==Jacobi==\n")
+    c_jacobi = jacobi(data.A3, data.b3, x0)
+    print("Solution for c): ", c_jacobi)
+    d_jacobi = jacobi(data.A4, data.b4, x0)
+    print("Solution for d): ", d_jacobi)
+    e_jacobi = jacobi(data.A5, data.b5, np.zeros_like(data.b5))
+    print("Solution for e): ", e_jacobi)
     print("\n")
 
-    x_jor = jor(data.A3, data.b3, x0, omega=0.8)
-    print("JOR solution: ", x_jor)
+    print("==JOR==\n")
+    c_jor = jor(data.A3, data.b3, x0, omega=0.8)
+    print("Solution for c): ", c_jor)
+    d_jor = jor(data.A4, data.b4, x0, omega=0.8)
+    print("Solution for d): ", d_jor)
     print("\n")
 
-    x_gs = gs(data.A2, data.b2, x0)
-    print("GS solution: ", x_gs)
+    print("==GS==\n")
+    x_gs = gs(data.A2, data.b2, x0, opt=1)
+    print("Solution for c): ", x_gs)
     print("\n")
 
-    x_gsb = gsb(data.A3, data.b3, x0)
-    print("GSb solution: ", x_gsb)
+    print("==GSb==\n")
+    c_gsb = gsb(data.A3, data.b3, x0, opt=1)
+    print("Solution for c): ", c_gsb)
+    d_gsb = gsb(data.A4, data.b4, x0, opt=1)
+    print("Solution for d): ", d_gsb)
+    print("\n")
+
+    print("==SOR==\n")
+    c_sor = sor(data.A3, data.b3, x0, omega=1.5)
+    print("Solution for c): ", c_sor)
+    d_sor = sor(data.A4, data.b4, x0, omega=1.5)
+    print("Solution for d): ", d_sor)
     print("\n")
